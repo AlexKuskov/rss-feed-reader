2f77ab4df22dba3338d52c75f9b01b1fcf58f862
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-06-03 08:32:16 +0300

    Reduced width of post-content section when it's shrinked

--- src/app/components/channel-posts/channel-posts.component.ts
+++ src/app/components/channel-posts/channel-posts.component.ts
@@ -54,6 +54,11 @@ export class ChannelPostsComponent implements OnInit {
     this.channelsService.getChannelDataById(indx).subscribe(channelData => {
       for (let i = 0; i < channelData["items"].length; i++) {
         this.channelPostTitle.push(channelData["items"][i]["title"]);
+        //added next code to emulate more than 10 items, because https://rss2json.com/ service doesn't allow
+        //to load more than 10 items at once
+        if (indx === 2) {
+          this.channelPostTitle.push(channelData["items"][i]["title"]);
+        }
       }
     });
   }
