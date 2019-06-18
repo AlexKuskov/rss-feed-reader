06481b54a101af4d95bbf76254a8deb1adf2084c
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-06-18 15:06:54 +0300

    Added export for each function in utils file

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,12 +1,10 @@
 import { Observable } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 
-export {errorHandler, distinct}
-
-function errorHandler(error: HttpErrorResponse): Observable<never> {
+export function errorHandler(error: HttpErrorResponse): Observable<never> {
   return Observable.throw(error.message || "Server Error");
 }
 
-function distinct<Item>(item: Item, index: number, array: Array<Item>): boolean {
+export function distinct<Item>(item: Item, index: number, array: Array<Item>): boolean {
   return array.indexOf(item) === index
 }
